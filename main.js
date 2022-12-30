4156a3c9724025361f3305334606c9ad6c66c1f1
Shusuke <ashleymusitelli@gmail.com>
2022-12-30 20:47:11 +0100

    Javascript added

---
+++ main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("div[data-id='Parchemin du shinobi'], div[data-id='Annonces'], div[data-id='Services'], div[data-id='Archives']").wrapAll('<div class="ash-categories"></div>');
+    $("div[data-id='Personnage'], div[data-id='Carnet du shinobi']").wrapAll('<div class="ash-categories"></div>');
+  });
+  
