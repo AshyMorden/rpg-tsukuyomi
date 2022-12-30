e10407f4f3922c04310e02f9ba639565ebe2122f
Shusuke <ashleymusitelli@gmail.com>
2022-12-30 23:18:57 +0100

    PA Fix

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("div[data-id='Parchemin du shinobi'], div[data-id='Annonces'], div[data-id='Services'], div[data-id='Archives']").wrapAll('<div class="ash-categories"></div>');
     $("div[data-id='Personnage'], div[data-id='Carnet du shinobi']").wrapAll('<div class="ash-categories"></div>');
-  });
-  
+    
+    const onlineChanged = $("#online").text().replace('Utilisateurs enregistrés :','');
+    $("#online").text(onlineChanged);
+
+    const totalOnlineText = $("#totalonline").text();
+    const totalOnlineParts = totalOnlineText.split(' :: ');
+    const totalOnlineChanged = totalOnlineParts[1].replace('Enregistré,','Shinobi')
+                                                 .replace('Invisible et','Yokai')
+                                                 .replace('Invité','Présents');
+    $("#totalonline").text(totalOnlineChanged);
+
+    
+    const connectedMembersChanged = $("#connectedmembers").text().replace('Membres connectés au cours des 24 dernières heures :','');
+    $("#connectedmembers").text(connectedMembersChanged);
+});
